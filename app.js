261275e7880e39d2218cfcc57bf91d933a222eac
franciscoguido-99 <francisconguido@gmail.com>
2020-12-11 21:03:59 -0300

    node

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/login", function(req,res){
+    res.sendFile(__dirname + "/views/login.html");
+});
+
+
+app.listen(3000, function(){console.log("Escuchando servidor 3000")});
