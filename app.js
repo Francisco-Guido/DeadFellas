507f81c3a10d57ac142efc947fd38eca56e9cee1
franciscoguido-99 <francisconguido@gmail.com>
2020-12-11 21:23:20 -0300

    cambios

--- app.js
+++ app.js
@@ -12,5 +12,21 @@ app.get("/login", function(req,res){
     res.sendFile(__dirname + "/views/login.html");
 });
 
+app.get("/productCart", function(req,res){
+    res.sendFile(__dirname + "/views/productCart.html");
+});
+
+app.get("/productDetail", function(req,res){
+    res.sendFile(__dirname + "/views/productDetail.html");
+});
+
+app.get("/products", function(req,res){
+    res.sendFile(__dirname + "/views/products.html");
+});
+
+app.get("/register", function(req,res){
+    res.sendFile(__dirname + "/views/register.html");
+});
+
 
 app.listen(3000, function(){console.log("Escuchando servidor 3000")});
