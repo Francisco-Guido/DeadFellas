a22fa0e2af052aa37381341c486ec69bdff13818
franciscoguido-99 <francisconguido@gmail.com>
2020-12-30 20:04:34 -0300

    cambios

--- source/app.js
+++ source/app.js
@@ -9,20 +9,20 @@ app.set('views', path.join(__dirname, './views'));
 app.use(express.static(path.join(__dirname, '../public') ));
 
 const generalRouter = require('./routers/indexRouter');
-const productsRouter = require('./routers/productsRouter');
-const productsDetailRouter = require('./routers/productsDetailRouter');
-const productsCartRouter = require('./routers/productsCartRouter');
-const loginRouter = require('./routers/loginRouter')
-const registerRouter = require('./routers/registerRouter');
+// const productsRouter = require('./routers/productsRouter');
+// const productsDetailRouter = require('./routers/productsDetailRouter');
+// const productsCartRouter = require('./routers/productsCartRouter');
+// const loginRouter = require('./routers/loginRouter')
+// const registerRouter = require('./routers/registerRouter');
 
 app.use(express.static(path.join(__dirname, '../public') ));
 
-app.use('/products', productsRouter);
+//app.use('/products', productsRouter);
 app.use('/', generalRouter);
-app.use('/productsDetail', productsDetailRouter);
-app.use('/productsCart', productsCartRouter);
-app.use('/login', loginRouter);
-app.use('/register', registerRouter);
+//app.use('/productsDetail', productsDetailRouter);
+//app.use('/productsCart', productsCartRouter);
+//app.use('/login', loginRouter);
+//app.use('/register', registerRouter);
 
 
 
