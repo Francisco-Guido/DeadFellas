40f562dca55d0a2beb32e8cc096680e7a60ecbe4
franciscoguido-99 <francisconguido@gmail.com>
2021-03-08 00:09:36 -0300

    CSS+HTML Vista modificar usuarios

---
+++ src/controllers/editUserController.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+const bcrypt=require("bcryptjs");
+const db = require ('../database/models/');
+
+module.exports = {
+
+    
+}
---
+++ src/routers/editUserRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const editUserController = require('../controllers/editUserController');
+const bcrypt=require("bcryptjs");
+
+
+
+
+
+
+
