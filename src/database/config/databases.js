c0abe1ad8a86e00dd4e7ad081a9c06f71c753bc4
franciscoguido-99 <francisconguido@gmail.com>
2021-03-13 17:34:29 -0300

    cambiando banner login

--- src/database/config/databases.js
+++ src/database/config/databases.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "pootismc",
+    "password": null,
     "database": "deadfellas_db",
     "host": "127.0.0.1",
     "dialect": "mysql",
@@ -9,7 +9,7 @@ module.exports = {
   },
   "test": {
     "username": "root",
-    "password": "pootismc",
+    "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
